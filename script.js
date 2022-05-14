f064302e93c748854dcd2833fde8028ba3fd4731
jimvonrgb <87355610+jimvonrgb@users.noreply.github.com>
2022-05-14 15:27:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const monke = document.getElementById("change-monke");
+const start_btn = document.getElementById("start");
+const stop_btn = document.getElementById("stop");
+
+var song = new Audio("./assets/monke.mp3");
+
+start_btn.addEventListener("click", function() {
+    monke.src = "./assets/spin.gif";
+    song.play();
+    song.loop = true
+})
+
+stop_btn.addEventListener("click", function() {
+    monke.src = "./assets/static.png";
+    song.pause();
+    song.loop = false;
+})
